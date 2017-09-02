44afeb71f6b128eee735955dbc4e09186a61e61a
Onchari Wycliffe <onchariwycliffe@gmail.com>
2017-09-02 13:23:43 +0300

    After first finish

---
+++ My_Expenses/My_Expenses/ExpenseItHome.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace My_Expenses
+{
+    /// <summary>
+    ///     Interaction logic for ExpenseItHome.xaml
+    /// </summary>
+    public partial class ExpenseItHome : Page
+    {
+        public ExpenseItHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+           //View Expenses Report
+           ExpenseReportPage expenseReportPage = new ExpenseReportPage();
+            this.NavigationService.Navigate(expenseReportPage);
+
+        }
+
+        private void ViewButton_OnClick(object sender, RoutedEventArgs e)
+        {
+          ExpenseReportPage expenseReportPage = new ExpenseReportPage(this.PeopleListBox.SelectedItem);
+            this.NavigationService.Navigate(expenseReportPage);
+        }
+    }
+}
---
+++ My_Expenses/My_Expenses/ExpenseReportPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace My_Expenses
+{
+    /// <summary>
+    ///     Interaction logic for ExpenseReportPage.xaml
+    /// </summary>
+    public partial class ExpenseReportPage : Page
+    {
+        public ExpenseReportPage()
+        {
+            InitializeComponent();
+        }
+
+        public ExpenseReportPage(object data) : this()
+        {
+            this.DataContext = data;
+        }
+    }
+}
